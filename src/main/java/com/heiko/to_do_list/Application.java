39910008b9752cfa5673f352a8170ed4f6b48e03
Yuri Heiko <yuriheiko@gmail.com>
2016-11-25 12:20:35 +0200

    Changed for war-file deploying

--- src/main/java/com/heiko/to_do_list/Application.java
+++ src/main/java/com/heiko/to_do_list/Application.java
@@ -3,13 +3,22 @@ package com.heiko.to_do_list;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
-public class Application {
+//public class Application { // For Jar
+public class Application  extends SpringBootServletInitializer { // For war
+
+    /* For making war file */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
